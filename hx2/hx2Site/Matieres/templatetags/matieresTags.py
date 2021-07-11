6ad23c02c0700f354e0b1b0add161358aea55380
Trick5t3r <86847896+Trick5t3r@users.noreply.github.com>
2021-07-11 13:05:46 +0200

    Update matieresTags.py

--- hx2/hx2Site/Matieres/templatetags/matieresTags.py
+++ hx2/hx2Site/Matieres/templatetags/matieresTags.py
@@ -1,9 +1,30 @@
 from django import template
+import re
+
+### Natural sort ###
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    Coded found here : https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text.grouper) ]
+
+####################
+
+
 
 register = template.Library()
 
 
 @register.filter
 def sort_list(lst):
-        return sorted(lst)
+        try:
+             return sorted(lst, key=natural_keys)
+
+        except:
+             print("An error occurred while sorting in matieresTags.py.")
+             return sorted(lst)
 
